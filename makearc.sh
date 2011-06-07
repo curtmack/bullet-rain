f243790746bcb167d92f40e0b48e23afd31d3c48
curtis.mackie@doane.edu <curtis.mackie@doane.edu@2acc3d93-256b-6c48-9532-fe312818912d>
2011-06-07 03:59:34 +0000

    I can't brain today, I have the dumb.

--- makearc.sh
+++ makearc.sh
@@ -5,10 +5,5 @@
 # replace with whatever you have that's compatible
 TAR = "tar -czf"
 
-# Set up the name of the archive
-BASENAME = `basename $1`
-DIRNAME = `dirname $1`
-ARCNAME = "${DIRNAME}/${BASENAME}.arc"
-
 # Perform compression
-${TAR} ${TARNAME} $1/*
+${TAR} $1.arc $1/*
